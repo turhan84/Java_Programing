2de784d875370fafc24c8f9ec9bb06f11e6e5d93
Turhan Ismailov <turhan.ismailov@gmail.com>
2023-12-23 23:09:49 +0000

    Day 29

---
+++ src/Day30_Inheritance/TypesOfInheritance/Teacher.java
@@ -0,0 +1,16 @@
+package Day30_Inheritance.TypesOfInheritance;
+
+import java.time.LocalDate;
+
+public class Teacher extends Employee{
+
+    public Teacher(String name, char gender, LocalDate DOB, String jobTitle, String employeeId, double salary) {
+        super(name, gender, DOB, jobTitle, employeeId, salary);
+    }
+
+    public void teach(){
+        System.out.println(getName() + " is teaching");
+    }
+
+
+}
